2f5408b367b6ede82ba87dc44363f85f3234f832
StepanMakhankov <githubstepan@gmail.com>
2022-12-21 03:48:30 +0300

    Added a mini game

--- githubProject1/src/Main.java
+++ githubProject1/src/Main.java
@@ -8,5 +8,24 @@ public class Main {
         System.out.println("Hello " + name + "!");
         System.out.println("What is your favorite color?");
         String color = scanner.nextLine();
+        System.out.println("Would you like to play a mini game?");
+        String answer = scanner.nextLine();
+        switch (answer){
+            case "yes":
+                System.out.println("I guessed a number from the 3rd number from 0 to 9. If you guess at least one, then you win.");
+                System.out.println("Your answer:");
+                String answer2 = scanner.nextLine();
+                switch (Integer.parseInt(answer2)){
+                    case 1, 6, 7:
+                        System.out.println("You won!");
+                        break;
+                    default:
+                        System.out.println("You lose!");
+                        break;
+                }
+            case "no":
+                System.out.println("Bye!");
+                break;
+        }
     }
 }
